5a90100eb089cec131ba4f067deccb788fac7900
Pawel <jankowskipawelxd@gmail.com>
2021-02-09 21:30:04 +0100

    Add equals method for ObjectId

--- src/main/java/ticket_service/model/ObjectId.java
+++ src/main/java/ticket_service/model/ObjectId.java
@@ -6,7 +6,7 @@ public class ObjectId {
   public static ObjectId generate() {
     var uuid = UUID.randomUUID();
     var rawId = uuid.toString().substring(0, 8);
-    var objectId = new ObjectId(rawId); 
+    var objectId = new ObjectId(rawId);
     return objectId;
   }
 
@@ -19,5 +19,9 @@ public class ObjectId {
 
   protected ObjectId(String id) {
     this.id = id;
+  }
+
+  public boolean equals(ObjectId obj) {
+    return this.id.equals(obj.id);
   }
 }
