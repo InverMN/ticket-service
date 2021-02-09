165715160a1c12d18c3cdf318c15f044fcc8c0a9
Pawel <jankowskipawelxd@gmail.com>
2021-02-09 23:01:33 +0100

    Modify findAll() method to return ArrayList instead of Array

--- src/main/java/ticket_service/repository/Repository.java
+++ src/main/java/ticket_service/repository/Repository.java
@@ -1,5 +1,7 @@
 package ticket_service.repository;
 
+import java.util.ArrayList;
+
 import ticket_service.model.Model;
 import ticket_service.model.ObjectId;
 
@@ -8,7 +10,7 @@ public interface Repository<M extends Model> {
   public void insertMany(M[] models);
   public boolean exists(ObjectId id);
   public M findOneById(ObjectId id);
-  public M[] findAll();
+  public ArrayList<M> findAll();
   public void deleteOneById(ObjectId id);
   public void deleteAll();
 }
